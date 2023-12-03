d35818cf53fb9e786e78e2e2dad0db08eee8e82b
arpitsahni04 <arpitsahni04@gmail.com>
2023-12-02 19:26:43 -0500

    minor changes and retain graph

--- train.sh
+++
@@ -1,2 +0,0 @@
-python3 train.py --num_gpus 2 --param_config 1 --batch_size 12 --save_path "./pretrained_weights/exp1"
-python3 train.py --num_gpus 2 --param_config 2 --batch_size 12 --save_path "./pretrained_weights/exp2"
